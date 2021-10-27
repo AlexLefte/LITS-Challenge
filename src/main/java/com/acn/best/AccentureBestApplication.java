e5b4ac443c4bbe46b0db449959b5ac97b6a81c9a
axbg <bisagalexstefan@gmail.com>
2021-10-27 23:47:33 +0300

    Initialized project

---
+++ src/main/java/com/acn/best/AccentureBestApplication.java
@@ -0,0 +1,11 @@
+package com.acn.best;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccentureBestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccentureBestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/acn/best/AccentureBestApplicationTests.java
@@ -0,0 +1,11 @@
+package com.acn.best;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccentureBestApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
